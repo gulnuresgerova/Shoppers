b2bcff5d079c97fbaca5f20f06d4133e565870a7
gulnuresgerova <gd6w8rtw0@code.edu.az>
2024-01-23 08:18:12 -0800

    class

--- shoppers/assets/js/form.js
+++ shoppers/assets/js/form.js
@@ -12,19 +12,19 @@ let arr = null;
 let arrCopy = null;
 
 async function getData() {
-    let res = await axios(`${BASE_URL}`);
-    arr= res.data;
-    arrCopy= structuredClone(arr);
-   console.log(res.data)
-    drawTable(res.data);
-  }
-  getData();
+  let res = await axios(`${BASE_URL}`);
+  arr = res.data;
+  arrCopy = structuredClone(arr);
+  console.log(res.data);
+  drawTable(res.data);
+}
+getData();
 
-  function drawTable(data) {
-    tbody.innerHTML = "";
-    data.forEach((el) => {
+function drawTable(data) {
+  tbody.innerHTML = "";
+  data.forEach((el) => {
     let tr = document.createElement("tr");
-      tr.innerHTML += `
+    tr.innerHTML += `
       <td>${el.id}</td>
       <td><img src="${el.img}" alt=""></td>
       <td>${el.title}</td>
@@ -35,77 +35,75 @@ async function getData() {
           
           
           `;
-          tbody.append(tr)
-    });
-  }
-  async function deleteElem(id, btn) {
-    try {
-      if (window.confirm("do you delete ??")) {
-        await axios.delete(`${BASE_URL}/${id}`);
-        btn.closest("tr").remove();
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  
-  sort.addEventListener("click" , function(){
-    let sorted;
-    if(this.innerText=="Asc"){
-        sorted = arr.sort((a,b) => a.title.localeCompare(b.title));
-        this.innerText="Desc"
-    }
-    else if(this.innerText=="Desc"){
-        sorted = arr.sort((a,b)=> b.title.localeCompare(a.title));
-        this.innerText="Default"
-    }
-    else{
-        this.innerText="Asc";
-        sorted=arrCopy;  }
-        drawTable(sorted)
-  })
-
-  search.addEventListener("input" , function(e){
-    let b = arr.filter((item)=>{
-        return item.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
-    })
-    drawTable(b);
-    console.log(b)
-  })
-
-
-
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
-    let obj = {
-      title: allInputs[1].value,
-      desc: allInputs[2].value,
-      price: allInputs[3].value,
-      img: `./assets/img/${allInputs[0].value.split("\\")[2]}`,
-    };
-  
-    if (!editId) {
-      if (
-        allInputs[0].value != "" && 
-        allInputs[1].value != "" &&
-        allInputs[2].value != "" &&
-        allInputs[3].value != ""
-      ) {
-        await axios.post(`${BASE_URL}`, obj);
-      } else {
-        alert("hamisini doldur ");
-      }
-    } else {
-      await axios.patch(`${BASE_URL}/${editId}`, obj);
-    }
+    tbody.append(tr);
   });
- 
-  async function editBtn(id) {
-    editId = id;
-    const res = await axios(`${BASE_URL}/${id}`);
-    // allInputs[1].value = res.data?.title;
-    allInputs[2].value = res.data?.desc;
-    allInputs[3].value = res.data?.price;
-
+}
+async function deleteElem(id, btn) {
+  try {
+    if (window.confirm("do you delete ??")) {
+      await axios.delete(`${BASE_URL}/${id}`);
+      btn.closest("tr").remove();
+    }
+  } catch (error) {
+    console.log(error);
   }
+}
+
+sort.addEventListener("click", function () {
+  let sorted;
+  if (this.innerText == "Asc") {
+    sorted = arr.sort((a, b) => a.title.localeCompare(b.title));
+    this.innerText = "Desc";
+  } else if (this.innerText == "Desc") {
+    sorted = arr.sort((a, b) => b.title.localeCompare(a.title));
+    this.innerText = "Default";
+  } else {
+    this.innerText = "Asc";
+    sorted = arrCopy;
+  }
+  drawTable(sorted);
+});
+
+search.addEventListener("keyup", function (e) {
+  console.log("hkhk");
+  let b = arr.filter((item) => {
+    return item.title
+      .toLocaleLowerCase()
+      .includes(e.target.value.toLocaleLowerCase());
+  });
+  drawTable(b);
+  console.log(b);
+});
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  let obj = {
+    title: allInputs[1].value,
+    desc: allInputs[2].value,
+    price: allInputs[3].value,
+    img: `./assets/img/${allInputs[0].value.split("\\")[2]}`,
+  };
+
+  if (!editId) {
+    if (
+      allInputs[0].value != "" &&
+      allInputs[1].value != "" &&
+      allInputs[2].value != "" &&
+      allInputs[3].value != ""
+    ) {
+      await axios.post(`${BASE_URL}`, obj);
+    } else {
+      alert("hamisini doldur ");
+    }
+  } else {
+    await axios.patch(`${BASE_URL}/${editId}`, obj);
+  }
+});
+
+async function editBtn(id) {
+  editId = id;
+  const res = await axios(`${BASE_URL}/${id}`);
+  // allInputs[1].value = res.data?.title;
+  allInputs[2].value = res.data?.desc;
+  allInputs[3].value = res.data?.price;
+}
